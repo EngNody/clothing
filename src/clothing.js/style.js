75ca4e7cff140c7db0f017b27bf39175438ebb5f
EngNody <totatota1990253@gmail.com>
2023-07-05 07:48:33 +0300

    .

--- src/clothing.js/style.js
+++ src/clothing.js/style.js
@@ -35,8 +35,27 @@ navbar();
 // ***********************************************************************
 
 
+const productcontainers=document.querySelectorAll('.product-container')
+const preptn=document.querySelectorAll('.pre-btn')
+const nextptn=document.querySelectorAll('.next-btn')
 
+productcontainers.forEach((item,i) => {
+  let containerdimenstions = item.getBoundingClientRect();
+  let containerwidth = containerdimenstions.width
 
+  nextptn[i].addEventListener('click', () => {
+    // item.scrollLeft += containerwidth
+    item.scrollLeft += (containerwidth-'500')
+  })
+
+  preptn[i].addEventListener('click', () => {
+        // item.scrollLeft += containerwidth
+    item.scrollLeft -= (containerwidth-'500')
+  })
+});
+
+// ***************************************************************
+  
 
 
 
