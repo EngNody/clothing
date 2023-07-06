44de85aa70906b74b3425d1c72f94467abbe8538
EngNody <totatota1990253@gmail.com>
2023-07-06 13:17:06 +0300

    .

--- src/clothing.js/style.js
+++ src/clothing.js/style.js
@@ -39,8 +39,8 @@ nav.innerHTML=`
 navbar();
 
 // ***********************************************************************
-
-
+// run arrows
+// ***********************************************************************
 const productcontainers=document.querySelectorAll('.product-container')
 const preptn=document.querySelectorAll('.pre-btn')
 const nextptn=document.querySelectorAll('.next-btn')
@@ -61,8 +61,72 @@ productcontainers.forEach((item,i) => {
 });
 
 // ***************************************************************
+// make footer
+// ***************************************************************
 
+const footer = () => {
+  
+let footer=document.querySelector('footer')
 
+footer.innerHTML=`
+
+<div class="footer-container">
+<div class="logocircle">
+    <img src="images/logo5.jpg" class="logo" alt="">
+</div> 
+ <div class="footer-ul-container">
+<ul class="category">
+  <li class="category-title">men</li>
+  <li><a href="" class="footer-link">t-shirts</a></li>
+  <li><a href="" class="footer-link">sweatshirts</a></li>
+  <li><a href="" class="footer-link">shirts</a></li>
+  <li><a href="" class="footer-link">jeans</a></li>
+  <li><a href="" class="footer-link">trousers</a></li>
+  <li><a href="" class="footer-link">shoes</a></li>
+  <li><a href="" class="footer-link">casuals</a></li>
+  <li><a href="" class="footer-link">formals</a></li>
+  <li><a href="" class="footer-link">sports</a></li>
+  <li><a href="" class="footer-link">watch</a></li>
+</ul>
+
+<ul class="category">
+  <li class="category-title">women</li>
+  <li><a href="" class="footer-link">t-shirts</a></li>
+  <li><a href="" class="footer-link">sweatshirts</a></li>
+  <li><a href="" class="footer-link">shirts</a></li>
+  <li><a href="" class="footer-link">jeans</a></li>
+  <li><a href="" class="footer-link">trousers</a></li>
+  <li><a href="" class="footer-link">shoes</a></li>
+  <li><a href="" class="footer-link">casuals</a></li>
+  <li><a href="" class="footer-link">formals</a></li>
+  <li><a href="" class="footer-link">sports</a></li>
+  <li><a href="" class="footer-link">watch</a></li>
+</ul>
+  </div>
+</div>
+<p class="footer-title">About Company</p>
+<p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, sint? Soluta nobis omnis officia repellendus porro necessitatibus, doloribus fugit voluptate cupiditate explicabo. Et praesentium molestias, culpa sit illo aspernatur sint?</p>
+<p class="info">support emails - help@clothing.com, 
+  customersupport@clothing.com</p>
+<p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+<div class="footer-social-container">
+  <div>
+    <a href="#" class="social-link">terms & services</a>
+    <a href="#" class="social-link">privacy page</a>
+  </div>
+  <div>
+    <a href="#" class="social-link">instagram</a>
+    <a href="#" class="social-link">facebook</a>
+    <a href="#" class="social-link">twitter</a>
+  </div>
+</div>
+<p class="footer-credit">Clothing, Best apperals online store</p>
+
+`
+
+}
+
+footer()
 
 
 
